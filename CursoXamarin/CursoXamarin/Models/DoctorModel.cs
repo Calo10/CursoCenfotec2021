82ea285a4aa3ed76c2cc7dce882bdbdd9f1a6d9d
Calo <carlos_mendez_10@live.com>
2021-07-12 00:58:50 -0600

    Sesion 2

---
+++ CursoXamarin/CursoXamarin/Models/DoctorModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CursoXamarin.Models
+{
+    public class DoctorModel
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Icon { get; set; }
+
+        public DoctorModel()
+        {
+        }
+    }
+}
---
+++ CursoXamarin/CursoXamarin/ViewModels/HomeViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using CursoXamarin.Models;
+
+namespace CursoXamarin.ViewModels
+{
+    public class HomeViewModel : INotifyPropertyChanged
+    {
+        #region Properties
+
+        private ObservableCollection<DoctorModel> _lstDoctors = new ObservableCollection<DoctorModel>();
+
+        public ObservableCollection<DoctorModel> lstDoctors
+        {
+            get
+            {
+                return _lstDoctors;
+            }
+            set
+            {
+                _lstDoctors = value;
+                OnPropertyChanged("lstDoctors");
+            }
+        }
+
+        #endregion
+
+
+
+
+        public HomeViewModel()
+        {
+            lstDoctors.Add(new DoctorModel { Id = "1", FirstName = "Joe", LastName = "Doe", Icon = "https://eshendetesia.com/images/user-profile.png" });
+            lstDoctors.Add(new DoctorModel { Id = "2", FirstName = "Nat", LastName = "Men", Icon = "" });
+            lstDoctors.Add(new DoctorModel { Id = "3", FirstName = "Sof", LastName = "Bar", Icon = "https://image.flaticon.com/icons/png/512/219/219990.png" });
+        }
+
+
+        #region PropertyChangedImplementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (propertyName != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
