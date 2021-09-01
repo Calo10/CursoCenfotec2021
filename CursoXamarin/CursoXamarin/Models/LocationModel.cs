97b7325dc0e6101c95f160905612b3acfd335763
Calo <carlos_mendez_10@live.com>
2021-08-31 19:18:35 -0600

    Add bindable bihaviour

--- CursoXamarin/CursoXamarin/Models/LocationModel.cs
+++ CursoXamarin/CursoXamarin/Models/LocationModel.cs
@@ -10,6 +10,10 @@ namespace CursoXamarin.Models
         public string Country { get; set; }
         public string Timezone { get; set; }
 
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Description { get; set; }
+
 
         public LocationModel()
         {
--- CursoXamarin/CursoXamarin/Views/Behavior/BindableBehavior.cs
+++ CursoXamarin/CursoXamarin/Views/Behavior/BindableBehavior.cs
@@ -8,8 +8,33 @@ namespace CursoXamarin.Views.Behavior
 
         public T AssociatedObject { get; private set; }
 
-        public BindableBehavior()
+
+        protected override void OnAttachedTo(T bindable)
         {
+            base.OnAttachedTo(bindable);
+
+            AssociatedObject = bindable;
+
+            if (bindable.BindingContext != null)
+                BindingContext = bindable.BindingContext;
+
+            bindable.BindingContextChanged += OnBindingContextChanged;
+        }
+
+        private void OnBindingContextChanged(object sender, EventArgs e)
+        {
+            OnBindingContextChanged();
+        }
+
+        protected override void OnDetachingFrom(T bindable)
+        {
+            bindable.BindingContextChanged -= OnBindingContextChanged;
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            BindingContext = AssociatedObject.BindingContext;
         }
     }
 }
